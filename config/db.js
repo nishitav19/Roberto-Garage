3adc3d99a04cf6697bb640b292936e1cc18b87a5
Nishita V <nishitavaddem@gmail.com>
2020-07-07 20:09:35 +0800

    Connecting Mongodb with mongoose

--- config/db.js
+++ config/db.js
@@ -4,8 +4,15 @@ const db = config.get("mongoURI")
 
 const connectDB = async () => {
     try {
-
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('MongoDB Connected');
     } catch (error) {
-
+        console.error(error.message)
+        process.exit(1)
     }
 }
+
+module.exports = connectDB
