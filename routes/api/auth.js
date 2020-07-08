8870bb85adbc9b81894b7bed806cf5af0bfcd972
Nishita V <nishitavaddem@gmail.com>
2020-07-08 09:21:42 +0800

    Routes and jwt authentication

---
+++ routes/api/auth.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const router = express.Router()
+const auth = require('../../middleware/auth')
+const { check, validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const bcrypt = require('bcryptjs')
+
+const User = require('../../models/User')
+
+// @route    GET api/auth
+// @desc     Test route
+// @access   Public
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route    POST api/auth
+// @desc     Authenticate user and get token
+// @access   Public
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { name, email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] })
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000 },
+            (err, token) => {
+                if (err) throw err
+                res.json({ token })
+            })
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server error')
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express()
 
 connectDB()
 
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => res.send('API Running'))
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
 
 const PORT = process.env.PORT || 5000
 
